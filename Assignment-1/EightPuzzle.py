d6386a3d2af77c6bf63869e190e591509bf6fce1
Anirudh <anirudh95@gmail.com>
2019-02-02 18:48:30 -0500

    initial first assignment 1

---
+++ Assignment-1/EightPuzzle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+class EightPuzzle:
+    def __init__(self):
+        self.board = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
+
+    def init_pos(self,positions):
+        print "hello"
+        #for i in positions:
+
