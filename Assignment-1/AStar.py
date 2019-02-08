fb8589e90f4cd432d73b549030905f58065aad34
Anirudh <anirudh95@gmail.com>
2019-02-08 12:50:32 -0500

    finished first working version

--- Assignment-1/AStar.py
+++ Assignment-1/AStar.py
@@ -14,27 +14,32 @@ class Solver:
         depth = 0
         now_node = Node(self.puzzle,self.goal,depth)
         self.pq.put(now_node)
+        explored = []
 
         steps = 0
         temp_exec = 0
-        while self.pq and temp_exec < 22:
+        while self.pq and temp_exec < 2000:
             nextchild = self.pq.get()
-            nextchild.myeightpuzzle.print_board()
-            print "-------"
+            if not nextchild.myeightpuzzle.board in explored:
+                depth = nextchild.gofx
+                nextchild.myeightpuzzle.print_board()
+                print "-------"
 
-            manhattan_cost = nextchild.myeightpuzzle.current_cost()
-            if manhattan_cost == 0:
-                return self.goal,steps
-            depth = depth + 1
-            for child in nextchild.next:
-                self.pq.put(Node(child,self.goal,depth))
+                manhattan_cost = nextchild.myeightpuzzle.current_cost()
+                if manhattan_cost == 0:
+                    return self.goal,steps
+                depth = depth + 1
+                for child in nextchild.next:
+                    self.pq.put(Node(child,self.goal,depth))
+                
+                temp_exec += 1
+                steps = steps + 1
+                explored.append(nextchild.myeightpuzzle.board)
+
+
+
+                    
+                    
             
-            temp_exec += 1
-            steps = steps + 1
-
-
-                
-                
-        
 
 
