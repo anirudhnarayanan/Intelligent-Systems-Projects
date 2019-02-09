e08047da608ce222dff3aca36cd831ba886d5111
Anirudh <anirudh95@gmail.com>
2019-02-08 20:36:40 -0500

    completed test script

---
+++ Assignment-1/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from AStar import Solver
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3: 
+        print "Usage %s <comma separated start state> <comma separated goal state> <heuristic 0/1 -> manhattan/misplaced tiles"%sys.argv[0]
+        sys.exit(-1)
+
+
+    k =0
+    positions = sys.argv[1].split(",")[:9]
+    goal = sys.argv[2].split(",")[:9]
+
+    positions = map(int,positions)
+    goal = map(int,goal)
+    input_states = [[None for i in range(3)] for j in range(3)]
+    for i in range(3):
+        for j in range(3):
+            input_states[i][j] = positions[k]
+
+            k+=1
+
+    print input_states
+    print goal
+    a = Solver(input_states,goal)
+    a.ASTAR()
+
+
+
+
+
+    
+        
